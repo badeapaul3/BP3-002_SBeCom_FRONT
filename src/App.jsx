f66f9e3e5da0e851420fd5abb9b807167ec98a86
Paul <badeapaul3@gmail.com>
2026-03-15 11:52:19 +0200

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import { FaBeer } from "react-icons/fa";
 
 
 function App() {
   
   return (
     <div>
-      
+      <h1 className='flex items-center justify-center h-screen bg-gray-800 text-white
+      text-2xl font-bold'>Welcome <FaBeer className= 'ml-3'></FaBeer> </h1>
     </div>
   );
 }
